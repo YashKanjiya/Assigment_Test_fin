59d9100594e4fc36b75d85321fc1e198c3d26b77
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-04-02 20:20:19 +0530

    First Project can add in github

---
+++ Backend/closebacked/src/main/java/com/example/closebacked/ClosebackedApplication.java
@@ -0,0 +1,13 @@
+package com.example.closebacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosebackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosebackedApplication.class, args);
+	}
+
+}

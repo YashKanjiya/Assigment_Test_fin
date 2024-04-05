3acefeea38450063df51e13036e05cf7cc3fde66
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-04-05 23:28:37 +0530

    Update axios_helper.js

--- Frontend/closefrontend/src/Api/helper/axios_helper.js
+++ Frontend/closefrontend/src/Api/helper/axios_helper.js
@@ -12,7 +12,7 @@ export const setAuthHeader = (token) => {
     }
 };
 
-axios.defaults.baseURL = 'http://localhost:9292';
+axios.defaults.baseURL = 'http://35.154.183.221:9292';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export const request = (method, url, data) => {
